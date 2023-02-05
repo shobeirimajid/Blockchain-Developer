bf77000638e409cb5847ac684103fcd5de6045ee
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-02-05 15:12:33 +0330

    Update ReceiveFallback.sol

--- Solidity-Advanced/1-ExternalCalls/ReceiveFallback.sol
+++ Solidity-Advanced/1-ExternalCalls/ReceiveFallback.sol
@@ -2,12 +2,26 @@
     Special Functions
         https://docs.soliditylang.org/en/v0.8.18/contracts.html#special-functions
 
+
+        "receive" Ether function
+            "Ether transfers"
+            Any call with "empty calldata"
+
+        payable "fallback" function
+            "interface confusions"
+            every call with "non-empty calldata"
+                even if Ether is sent along with the call
+            
+*/
+
+
+
+/**
     Receive Ether Function
 
-        A contract can have at most one receive function, declared using :
+        A contract can have at most one receive function, without the function keyword, declared using :
 
-            receive() external payable { ... } 
-                without the function keyword. 
+            receive() external payable { ... }  
 
         This function:
             - cannot have arguments
@@ -69,7 +83,189 @@
                 as a recipient of a "coinbase transaction" (miner block reward) 
                 or as a destination of a "selfdestruct"
 
-                A contract cannot react to such Ether transfers and thus also cannot reject them. This is a design choice of the EVM and Solidity cannot work around it.
+                A contract cannot react to such Ether transfers and thus also cannot reject them. 
+                    This is a design choice of the EVM and Solidity cannot work around it.
 
-It also means that address(this).balance can be higher than the sum of some manual accounting implemented in a contract (i.e. having a counter updated in the receive Ether function).    
+                It also means that address(this).balance can be higher than the sum of some manual accounting implemented in a contract 
+                    i.e. having a counter updated in the receive Ether function.
 */
+
+
+/// Below you can see an example of a Sink contract that uses function receive.
+/// This contract keeps all Ether sent to it with no way to get it back.
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.0 <0.9.0;
+
+contract Sink {
+
+    event Received(address, uint);
+
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+}
+
+
+
+/**
+    Fallback Function
+
+        A contract can have at most one "fallback function", without the "function keyword", declared using : 
+
+            fallback () external [payable] 
+            fallback (bytes calldata input) external [payable] returns (bytes memory output) 
+        
+        
+        fallback function:
+            - must have "external" visibility
+            - can be "virtual"
+            - can "override" 
+            - can have "modifiers"
+            - always receives data
+            - must be marked payable (in order to also receive Ether)
+
+
+        The "fallback" function is executed on a call to the contract :
+            - if none of the other functions match the given "function signature"
+            - if "no data" was supplied at all and there is "no receive Ether function"
+
+
+        If the version with parameters is used, input will contain the "full data sent to the contract" - equal to "msg.data"
+            and can return data in output.
+        
+        The returned data will not be ABI-encoded. 
+            Instead it will be returned without modifications (not even padding).
+
+
+        In the worst case, if a payable fallback function is also used in place of a receive function, 
+            it can only rely on 2300 gas being available (see receive Ether function).
+
+        Like any function, the fallback function can execute complex operations as long as there is enough gas passed on to it.
+
+
+        Warning:
+
+            A payable fallback function is also executed for "plain Ether transfers", 
+                if no receive Ether function is present. 
+                
+            It is recommended to always 
+                define a "receive" Ether function as well, 
+                if you define a payable fallback function to distinguish "Ether transfers" from "interface confusions".
+
+
+        Note:
+            If you want to "decode the input data",
+                you can check the "first four bytes" for the "function selector" 
+                and then you can use "abi.decode" together with the "array slice syntax" to "decode ABI-encoded data" : 
+
+                    (c, d) = abi.decode(input[4:], (uint256, uint256)); 
+
+                Note that this should only be used as a last resort and proper functions should be used instead.
+*/
+
+
+
+
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.2 <0.9.0;
+
+contract Test {
+
+    uint x;
+
+    // This function is called for all messages sent to this contract (there is no other function).
+    // Sending Ether to this contract will cause an exception, 
+    //    because the fallback function does not have the `payable` modifier.
+    
+    fallback() external { 
+        x = 1; 
+    }
+}
+
+
+
+
+
+contract TestPayable {
+
+    uint x;
+    uint y;
+
+
+    //    This function is called for all messages sent to this contract, except plain Ether transfers 
+    //        because there is no other function except the receive function.
+
+    //    Any "call" with "non-empty calldata" to this contract will execute the fallback function
+    //        even if Ether is sent along with the call.
+    
+    fallback() external payable {
+         x = 1; 
+         y = msg.value; 
+    }
+
+
+
+    //    This function is called for plain Ether transfers, 
+    //        i.e. for every call with "empty calldata".
+
+    receive() external payable { 
+        x = 2; 
+        y = msg.value; 
+    }
+}
+
+
+
+contract Caller {
+
+
+    function callTest(Test test) public returns (bool) {
+
+        // fallback function will be executed
+        // and results in (test.x = 1)
+        (bool success,) = address(test).call(abi.encodeWithSignature("nonExistingFunction()"));
+        require(success);
+        
+
+        // address(test) will not allow to call 'send' directly, 
+        //    since 'test' has no payable fallback function
+        // It has to be converted to the 'address payable' type to even allow calling 'send' on it
+        address payable testPayable = payable(address(test));
+
+
+        // There is no receive function in Test contract
+        // If someone sends Ether to that contract, the transfer will fail
+        //    i.e. this returns false here.
+        return testPayable.send(2 ether);
+    }
+
+
+
+    function callTestPayable(TestPayable test) public returns (bool) {
+
+        // fallback function will be executed
+        // results in (test.x= 1) and (test.y = 0)
+        (bool success,) = address(test).call(abi.encodeWithSignature("nonExistingFunction()"));
+        require(success);
+        
+
+        // fallback function will be executed
+        // results in (test.x = 1) and (test.y = 1)
+        (success,) = address(test).call{value: 1}(abi.encodeWithSignature("nonExistingFunction()"));
+        require(success);
+        
+
+        // "receive" function will be called.
+        // Since that function writes to storage, it takes more gas than is available with a simple `send` or `transfer`.
+        // Because of that, we have to use a "low-level call"
+        // results in (test.x = 2) and (test.y = 2 ether)
+        (success,) = address(test).call{value: 2 ether}("");
+        require(success);
+        
+        
+        return true;
+    }
+
+}
