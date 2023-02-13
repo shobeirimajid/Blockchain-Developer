743941471571517d20c9d4e024ab15a31fc34705
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-02-13 20:30:18 +0330

    Update ex2-ChainLink-VRF-DirectFunding.sol

--- 2-Solidity-Advanced/2-Patterns/7-Oracles/ex2-ChainLink-VRF-DirectFunding.sol
+++ 2-Solidity-Advanced/2-Patterns/7-Oracles/ex2-ChainLink-VRF-DirectFunding.sol
@@ -48,18 +48,27 @@ contract VRFv2DirectFundingConsumer is VRFV2WrapperConsumerBase, ConfirmedOwner 
     // Cannot exceed VRFV2Wrapper.getConfig().maxNumWords.
     uint32 numWords = 2;
 
+    uint256 min;
+    uint256 max;
+
     // Address LINK - hardcoded for Goerli
     address linkAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
 
     // address WRAPPER - hardcoded for Goerli
     address wrapperAddress = 0x708701a1DfF4f478de54383E49a627eD4852C816;
 
+    constructor() ConfirmedOwner(msg.sender) VRFV2WrapperConsumerBase(linkAddress, wrapperAddress) {}
 
-    constructor() ConfirmedOwner(msg.sender) VRFV2WrapperConsumerBase(linkAddress, wrapperAddress) {
-
+    
+    function setInterval(uint _min, uint _max) public {
+        min = _min;
+        max = _max;
     }
 
 
+    // to send the request for random values to Chainlink VRF
+    // Remix IDE doesn’t set the right gas limit. 
+    // For this example to work, set a gas limit of 400,000
     function requestRandomWords() external onlyOwner returns (uint256 requestId) {
 
         requestId = requestRandomness(callbackGasLimit, requestConfirmations, numWords);
@@ -78,9 +87,19 @@ contract VRFv2DirectFundingConsumer is VRFV2WrapperConsumerBase, ConfirmedOwner 
     }
 
 
+    /* 
+        when the requested random words had been ready,
+        oracle returns the random values to your contract by calling fulfillRandomWords() function 
+    */
     function fulfillRandomWords(uint256 _requestId, uint256[] memory _randomWords) internal override {
 
         require(s_requests[_requestId].paid > 0, "request not found");
+
+            // get a random number in a range from "min" to "max"
+            // You have to set "min" , "max" via setInterval(), and call it before calling the requestRandomWords()
+            // if you don't want that, simply comment following 2 lines
+            for(uint i=0; i<_randomWords.length; i++)
+                _randomWords[i] = (_randomWords[i] % max) + min;
 
         s_requests[_requestId].fulfilled = true;
         s_requests[_requestId].randomWords = _randomWords;
@@ -89,9 +108,18 @@ contract VRFv2DirectFundingConsumer is VRFV2WrapperConsumerBase, ConfirmedOwner 
     }
 
 
-    function getRequestStatus(uint256 _requestId) external view
-        returns (uint256 paid, bool fulfilled, uint256[] memory randomWords)
-    {
+    /*
+        lastRequestId() => requestID of your request
+
+                                *** Waiting for random values ***
+            After the oracle returns the random values to your contract, the mapping s_requests is updated. 
+            The received random values are stored in "s_requests[_requestId].randomWords"
+            it might take a few minutes for the callback to return the requested random values to your contract.
+
+        getRequestStatus(_requestId) => display the "random words"
+    */
+    function getRequestStatus(uint256 _requestId) external view returns (uint256 paid, bool fulfilled, uint256[] memory randomWords) {
+
         require(s_requests[_requestId].paid > 0, "request not found");
 
         RequestStatus memory request = s_requests[_requestId];
@@ -100,9 +128,9 @@ contract VRFv2DirectFundingConsumer is VRFV2WrapperConsumerBase, ConfirmedOwner 
     }
 
 
-    /**
-     * Allow withdraw of Link tokens from the contract
-     */
+    /*
+        Allow withdraw of Link tokens from the contract
+    */
     function withdrawLink() public onlyOwner {
 
         LinkTokenInterface link = LinkTokenInterface(linkAddress);
@@ -110,3 +138,37 @@ contract VRFv2DirectFundingConsumer is VRFV2WrapperConsumerBase, ConfirmedOwner 
         require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
     }
 }
+
+
+
+
+/*
+    ----------------
+    Run
+    -----------------
+
+    1. Deploy the VRFv2DirectFundingConsumer
+
+    2. Fund VRFv2DirectFundingConsumer with LINK Token. (https://faucets.chain.link/)
+
+    3. call the "requestRandomWords()" function to send the request for random values to Chainlink VRF. 
+        Note: Remix IDE doesn’t set the right gas limit. 
+        For this example to work, set a gas limit of 400,000
+
+    4. To fetch the request ID of your request, call "lastRequestId()"
+        Ex: 99440127728384319498007479344467888154374567530587929257123447098566467056408 
+
+    5. After the oracle returns the random values to your contract, the mapping s_requests is updated. 
+        The received random values are stored in "s_requests[_requestId].randomWords"
+        it might take a few minutes for the callback to return the requested random values to your contract.
+        after few minutes, Call "getRequestStatus(_requestId)" and specify the requestId to display the "random words".
+        
+        Ex: randomWords[] = 35247896544343688172710550311178075338522545443371427762979831633407207410139,
+                            38632492572344245840875706881068213571861914207807667236715005213067594171074
+
+    7. After you are done with this contract, you can retrieve the remaining testnet LINK to use with other examples.
+        Call withdrawLink() function. 
+        MetaMask opens and asks you to confirm the transaction. 
+        After you approve the transaction, the remaining LINK will be transfered from your consuming contract 
+            to your wallet address.
+*/
