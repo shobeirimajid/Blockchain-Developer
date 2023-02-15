ab990d2c57fdf5aeb1843fc8a31db64d4222a447
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-02-15 20:46:00 +0330

    +

---
+++ 3-DeFi/Dex/Uniswap/UniswapV2-Addresses.sol
@@ -0,0 +1,47 @@
+/*
+    --------------------------
+    Uniswap V2 on Ethereum
+    --------------------------
+    https://info.uniswap.org/
+
+
+    UniswapV2Factory                0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f
+    UniswapV2Router                 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
+
+    -------------
+    Pairs
+    -------------
+    ETH / USDT          0.05%       0x11b815efb8f581194ae79006d24e0d814b7697f6
+    ETH / USDT          0.3%        0x4e68ccd3e89f51c3074ca5072bbac773960dfa36
+    DAI / ETH           0.05%       0x60594a405d53811d3bc4766596efd80fd545a270
+    DAI / ETH           0.3%        0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8
+    DAI / USDC          0.01%       0x5777d92f208679db4b9778590fa3cab3ac9e2168
+    DAI / USDC          0.05%       0x6c6bc977e13df9b0de53b251522280bb72383700
+    USDC / ETH          0.01%       0xe0554a476a092703abdb3ef35c80e0d76d32939f
+    USDC / ETH          0.05%       0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640
+    USDC / ETH          0.3%        0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8
+    USDC / USDT         0.01%       0x3416cf6c708da44db2624d63ea0aaef7113527c6
+    USDC / USDT         0.05%       0x7858e59e0c01ea06df3af3d20ac7b0003275d4bf
+    WBTC / ETH          0.05%       0x4585fe77225b41b697c938b018e2ac67ac5a20c0
+    WBTC / ETH          0.3%        0xcbcdf9626bc03e24f779434178a73a0b4bad62ed
+    WBTC / USDC         0.3%        0x99ac8ca7087fa4a2a1fb6357269965a2014abc35
+    UNI / ETH           0.3%        0x1d42064fc4beb5f8aaf85f4617ae8b3b5b8bd801
+    LINK / ETH          0.3%        0xa6cc3c2531fdaa6ae1a3ca84c2855806728693e8
+    MATIC / ETH         0.3%        0x290a6a7460b308ee3f19023d2d00de604bcf5b42
+
+    -------------
+    Tokens
+    -------------
+    ETH-WETH                        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+    Uniswap (UNI)                   0x1f9840a85d5af5bf1d1762f925bdaddc4201f984
+    ChainLink Token (LINK)          0x514910771af9ca656af840dff83e8264ecf986ca
+    Matic Token (MATIC)             0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0
+    Wrapped BTC (WBTC)              0x2260fac5e5542a773aa44fbcfedf7c193bc2c599
+    Wrapped Doge (WDOGE)            0x35a532d376ffd9a705d0bb319532837337a398e7
+    Dai Stablecoin (DAI)            0x6b175474e89094c44da98b954eedeac495271d0f
+    Binance USD (BUSD)              0x4fabb145d64652a948d72533023f6e7a623c7c53
+    Tether USD (USDT)               0xdac17f958d2ee523a2206206994597c13d831ec7
+    USD Coin (USDC)                 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 
+
+
+*/
