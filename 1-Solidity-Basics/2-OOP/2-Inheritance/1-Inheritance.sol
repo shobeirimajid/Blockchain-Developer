cb75e94a34c731f5e4313b776036c725394d35a8
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-02-19 17:23:11 +0330

    +

--- 1-Solidity-Basics/2-OOP/2-Inheritance/1-Inheritance.sol
+++ 1-Solidity-Basics/2-OOP/2-Inheritance/1-Inheritance.sol
@@ -4,7 +4,58 @@
     ---------------
     https://docs.soliditylang.org/en/latest/contracts.html#inheritance
     
-    Solidity supports multiple inheritance including polymorphism.
+    ---------------
+    Polymorphism
+    ---------------
+    Solidity supports multiple inheritance including "polymorphism"
+
+    Polymorphism means that a function call (internal and external) 
+    always executes the function of the same signature (same name and parameter types)
+      in the most derived contract in the inheritance hierarchy. 
+
+    This has to be explicitly enabled on each function in the hierarchy 
+        using the 'virtual' and 'override' keywords. 
+
+    More details about "Function Overriding" are provided in the following.
+
+    If you want to call the function one level higher up in the flattened inheritance hierarchy
+        it is possible to call functions further up in the inheritance hierarchy internally
+        by explicitly specifying the contract using :
+
+            ContractName.functionName() or 
+            super.functionName() 
+
+
+    When a contract inherits from other contracts, 
+        only a single contract is created on the blockchain, 
+        and the code from all the base contracts is compiled into the created contract. 
+    
+        This means that all 'internal calls' to functions of base contracts 
+        also just use internal function calls 
+
+        Note: super.f() will use JUMP and not a message call
+
+
+    ------------------------
+    State variable shadowing
+    ------------------------
+    State variable shadowing is considered as an error. 
+    A derived contract can only declare a state variable x, 
+        if there is no visible state variable with the same name in any of its bases.
+
+      
+    The general inheritance system is very similar to Python’s
+        https://docs.python.org/3/tutorial/classes.html#inheritance
+    especially concerning multiple inheritance
+    but there are also some differences
+        https://docs.soliditylang.org/en/latest/contracts.html#multi-inheritance
+
+        
+
+
+
+    
+
 
 
 */
---
+++ 1-Solidity-Basics/2-OOP/2-Inheritance/ex1-inheritance.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract Owned {
+
+    address payable owner;
+
+    constructor() { 
+        owner = payable(msg.sender); 
+    }
+}
+
+
+
+/*
+    ---------------------
+    Dreved is Base
+    ---------------------
+    Use `is` to derive from another contract.
+    Derived contracts can access all `non-private` members including:
+        - internal functions 
+        - state variables
+    These cannot be accessed externally via `this`
+*/
+
+contract Destructible is Owned {
+
+    /*
+        ---------------------
+        virtual function
+        ---------------------
+        The keyword `virtual` means that the function can change its behaviour in derived contracts.
+    */
+
+    function destroy() virtual public {
+        if (msg.sender == owner) 
+            // This will report a warning due to deprecated selfdestruct
+            selfdestruct(owner);
+    }
+}
+
+
+
+/*
+    ---------------------
+    abstract contracts
+    ---------------------
+    abstract contracts are only provided to make the interface known to the compiler.
+    Note the function without body.
+    If a contract does not implement all functions, it can only be used as an interface.
+*/
+
+abstract contract Config {
+    function lookup(uint id) public virtual returns (address adr);
+}
+
+
+abstract contract NameReg {
+    function register(bytes32 name) public virtual;
+    function unregister() public virtual;
+}
+
+
+
+/*
+    ---------------------
+    Multiple inheritance
+    ---------------------
+    Multiple inheritance is possible in solidity.
+    Note that `Owned` is also a base class of `Destructible`,
+    yet there is only a single instance of `Owned` (as for virtual inheritance in C++)
+*/
+
+contract Named is Owned, Destructible {
+
+    constructor(bytes32 name) {
+        Config config = Config(0xD5f9D8D94886E70b06E474c3fB14Fd43E2f23970);
+        NameReg(config.lookup(1)).register(name);
+    }
+
+
+    /*
+        ---------------------
+        function overriding
+        ---------------------
+        Functions can be overridden by another function with the same signature
+            (same name and the same number/types of input parameters)
+
+        If the overriding function has different types of output parameters, that causes an error.
+
+        Both local and message-based function calls take these overrides into account.
+    
+        ---------------------------------------
+        `override` & `virtual` keywords
+        ---------------------------------------
+        If you want the function to override, you need to use the `override` keyword.
+        You need to specify the `virtual` keyword again if you want this function to be overridden again.
+    */
+
+    function destroy() public virtual override {
+
+        if (msg.sender == owner) {
+
+            Config config = Config(0xD5f9D8D94886E70b06E474c3fB14Fd43E2f23970);
+            NameReg(config.lookup(1)).unregister();
+
+            // It is still possible to call a specific overridden function.
+            Destructible.destroy();
+        }
+    }
+}
+
+
+
+/*
+    ------------------------------
+    base constructor's arguments
+    ------------------------------
+    If a base contract's constructor takes an argument, 
+    it needs to be provided 
+        - in the header  
+        - or in modifier-invocation-style at the constructor of the derived contract
+*/
+
+contract PriceFeed is Owned, Destructible, Named("GoldFeed") {
+
+    uint info;
+
+    function updateInfo(uint newInfo) public {
+        if (msg.sender == owner) 
+            info = newInfo;
+    }
+
+
+    /*
+        Here, we only specify `override` and not `virtual`.
+        This means that contracts deriving from `PriceFeed`
+        cannot change the behaviour of `destroy` anymore.
+    */
+
+    function destroy() public override(Destructible, Named) { 
+        Named.destroy();
+    }
+
+
+    function get() public view returns(uint r) { 
+        return info; 
+    }
+}
