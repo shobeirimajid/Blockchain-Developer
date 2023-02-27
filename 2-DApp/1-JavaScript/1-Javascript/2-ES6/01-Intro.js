8db9b82bd42ebae3a17575a3c3d849324b97b91e
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-02-27 16:49:30 +0330

    +

--- 2-DApp/1-JavaScript/1-Javascript/2-ES6/01-Intro.js
+++ 2-DApp/1-JavaScript/1-Javascript/2-ES6/01-Intro.js
@@ -3,32 +3,6 @@
 //                   EcmaScript
 ///////////////////////////////////////////////////////
 
------------------------
-JavaScript versions
------------------------
-The Original JavaScript ES1 ES2 ES3 (1997-1999)
-The First Main Revision ES5 (2009)
-The Second Revision ES6 (2015)
-All Yearly Additions (2016, 2017, 2018, 2019, 2020)
-
-
-avaScript/ECMAScript was invented by Brendan Eich in 1995.
-
-It was developed for Netscape 2, and became the ECMA-262 standard in 1997.
-
-After Netscape handed JavaScript over to ECMA, 
-    the Mozilla foundation continued to develop JavaScript for the Firefox browser. 
-    Mozilla's latest version was 1.8.5. (Identical to ES5).
-
-Internet Explorer (IE4) was the first browser to support ECMA-262 Edition 1 (ES1).
-
-* Internet Explorer 9 did not support ES5 "use strict".
-* Internet Explorer 11 does not support ES6.
-
-
-
-ECMAScript (ES) is a scripting language specification created to standardize JavaScript.
-
 The Sixth Edition, 
     initially known as ECMAScript 6 (ES6) 
     and later renamed to ECMAScript 2015, 
@@ -52,4 +26,5 @@ including :
 In other words, ES6 is a superset of JavaScript (ES5). 
 The reason that ES6 became so popular is that 
 it introduced new conventions and OOP concepts such as classes.
+
 */
