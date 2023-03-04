1c9ccefc0a73b1e2b41b9645867ac6bfc8cc9427
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-03-04 16:53:48 +0330

    +

--- 2-DApp/1-JavaScript/1-Javascript/1-Crash-Course/13-Loop.js
+++ 2-DApp/1-JavaScript/1-Javascript/1-Crash-Course/13-Loop.js
@@ -2,26 +2,29 @@
 //                 Loop
 ///////////////////////////////////////////////////////
 
-//----------------
-//  while
-//----------------
+// Loops are handy, if you want to run the same code over and over again, each time with a different value.
 
-while (true){
-    // An infinite loop!
+// Instead of writing:
+text += cars[0] + "<br>";
+text += cars[1] + "<br>";
+text += cars[2] + "<br>";
+text += cars[3] + "<br>";
+text += cars[4] + "<br>";
+text += cars[5] + "<br>";
+
+// You can write:
+for (let i = 0; i < cars.length; i++) {
+    text += cars[i] + "<br>";
 }
 
 
-//----------------
-//  do-while
-//----------------
+// Different Kinds of Loops
 
-// Do-while loops are like while loops, except they always run at least once.
-
-var input;
-do {
-    input = getInput();
-} while (!isValid(input));
-
+//  for        loops through a block of code a number of times
+//  for/in     loops through the properties of an object
+//  for/of     loops through the values of an iterable object
+//  while      loops through a block of code while a specified condition is true
+//  do/while   loops through a block of code while a specified condition is true
 
 
 //----------------
@@ -37,7 +40,7 @@ for (var i = 0; i < 5; i++){
 
     
 //----------------
-//  for/in
+//  for-in
 //----------------
 // The for/in statement allows iteration over properties of an object.
 
@@ -50,7 +53,7 @@ for (var x in person){
 
 
 //----------------
-//  for/of
+//  for-of
 //----------------
 // The for/of statement allows iteration over iterable objects 
 // including the built-in String, Array e.g. the Array-like arguments
@@ -78,6 +81,30 @@ for (var i = 0; i < 10; i++) {
         }
     }
 }
+
+
+
+//----------------
+//  while
+//----------------
+
+while (true){
+    // An infinite loop!
+}
+
+
+
+//----------------
+//  do-while
+//----------------
+
+// Do-while loops are like while loops, except they always run at least once.
+
+var input;
+do {
+    input = getInput();
+} while (!isValid(input));
+
 
 
 //----------------
