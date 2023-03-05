6c13eea5087fcdfb57c1632765cdf1724f9a9dab
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-03-05 18:01:17 +0330

    +

--- 2-DApp/1-JavaScript/1-Javascript/2-ES6/02-2-Hoisting.js
+++ 2-DApp/1-JavaScript/1-Javascript/2-ES6/02-2-Hoisting.js
@@ -79,8 +79,231 @@ const myCar;
 // Since this is how JavaScript interprets the code, it is always a good rule.
 
 
+// JavaScript in `strict mode` does not allow variables to be used if they are not declared.
+
+
+
 
 //-----------------------------------------
 // Strict Mode
 //-----------------------------------------
-// JavaScript in `strict mode` does not allow variables to be used if they are not declared.
+/*
+
+Strict mode makes it easier to write "secure" and "cleaner" JavaScript codes.
+Strict mode changes previously accepted "bad syntax" into real errors.
+
+Strict Mode indicates that JavaScript code should be executed in a limited mode.
+
+As an example, in normal JavaScript, mistyping a variable name creates a new global variable. 
+In strict mode, this will throw an error, making it impossible to accidentally create a global variable.
+
+In normal JavaScript, a developer will not receive any error feedback assigning values to non-writable properties.
+With strict mode, you can not assign value to undeclared variables.
+
+In strict mode, any assignment to one of the followings, will throw an error :
+
+    - non-writable property
+    - getter-only property
+    - non-existing property
+    - non-existing variable
+    - non-existing object 
+*/
+
+
+//-----------------------------------------
+// Browsers Compatibility
+//-----------------------------------------
+
+// "use strict" was new in ECMAScript version 5.
+// "use strict" only matters to new compilers that "understand" the meaning of it.
+// All modern browsers 
+
+// IE 9 and lower don't support "use strict"
+// but they will not throw an error even if it does not understand it.
+
+// Because the syntax, for declaring strict mode, was designed to be compatible with older versions of JavaScript.
+// "use strict" is not a statement, but a literal expression and is just a string 
+// so just simply ignored by earlier versions of JavaScript 
+
+
+
+//---------------------------
+// Declaring Strict Mode
+//---------------------------
+
+// The "use strict" directive is only recognized at the beginning of a `script` or a `function`
+
+// Strict mode is declared by adding "use strict";
+"use strict";
+
+
+
+// Declared at the beginning of a script, it has global scope 
+//  (all code in the script will execute in strict mode):
+
+"use strict";
+x = 3.14;       // This will cause an error because x is not declared
+
+
+"use strict";
+myFunction();
+
+function myFunction() {
+  y = 3.14;   // This will also cause an error because y is not declared
+}
+
+
+// Declared inside a function, it has local scope 
+// (only the code inside the function is in strict mode):
+
+x = 3.14;       // This will not cause an error.
+myFunction();
+
+function myFunction() {
+  "use strict";
+  y = 3.14;   // This will cause an error
+}
+
+
+//-----------------------------
+// Not Allowed in Strict Mode
+//-----------------------------
+
+// 1. Using a variable or object, without declaring it
+"use strict";
+x = 3.14;                // This will cause an error
+
+"use strict";
+x = {p1:10, p2:20};      // This will cause an error
+
+
+// 2. Deleting a variable (or object) is not allowed.
+"use strict";
+let x = 3.14;
+delete x;                // This will cause an error
+
+
+// 3. Deleting a function is not allowed.
+"use strict";
+function x(p1, p2) {};
+delete x;                // This will cause an error 
+
+
+// 4. Duplicating a parameter name is not allowed:
+"use strict";
+function x(p1, p1) {};   // This will cause an error
+
+
+// 5. Octal numeric literals are not allowed:
+"use strict";
+let x = 010;             // This will cause an error
+
+
+// 6. Octal escape characters are not allowed:
+"use strict";
+let x = "\010";            // This will cause an error
+
+
+// 7. Writing to a read-only property is not allowed:
+"use strict";
+const obj = {};
+Object.defineProperty(obj, "x", {value:0, writable:false});
+
+obj.x = 3.14;            // This will cause an error
+
+
+// 8. Writing to a get-only property is not allowed:
+"use strict";
+const obj = {get x() {return 0} };
+
+obj.x = 3.14;            // This will cause an error
+
+
+// 9. Deleting an undeletable property is not allowed:
+"use strict";
+delete Object.prototype; // This will cause an error
+
+
+// 10. The word eval cannot be used as a variable:
+"use strict";
+let eval = 3.14;         // This will cause an error
+
+
+// 11. The word arguments cannot be used as a variable:
+"use strict";
+let arguments = 3.14;    // This will cause an error
+
+
+// 12. The with statement is not allowed:
+"use strict";
+with (Math){x = cos(2)}; // This will cause an error
+
+
+// 13. eval() is not allowed to create variables in the scope from which it was called.
+//     For security reasons
+
+// In strict mode, a variable can not be used before it is declared:
+"use strict";
+eval ("x = 2");
+alert (x);      // This will cause an error
+
+
+// In strict mode, eval() can not declare a variable using the var keyword:
+"use strict";
+eval ("var x = 2");
+alert (x);    // This will cause an error
+
+// eval() can not declare a variable using the let keyword:
+eval ("let x = 2");
+alert (x);        // This will cause an error
+
+
+
+//-----------------------------
+// this in Strict Mode
+//-----------------------------
+
+// The `this` keyword refers to the object that called the function.
+
+// In strict mode, this in functions behaves differently
+
+// If the object is not specified :
+
+//    - In Normal Mode:
+//      functions will return the `global object` (window)
+
+//    - In Strict Mode:       
+//      functions will return `undefined` 
+
+"use strict";
+function myFunction() {
+  alert(this); // will alert "undefined"
+}
+myFunction();
+
+
+
+
+
+//-----------------------------
+// Future Proof!
+//-----------------------------
+/*
+
+Keywords reserved for future JavaScript versions can NOT be used as variable names in strict mode.
+
+    These are:
+
+        - implements
+        - interface
+        - let
+        - package
+        - private
+        - protected
+        - public
+        - static
+        - yield
+*/
+
+"use strict";
+let public = 1500;      // This will cause an error
---
+++ 2-DApp/1-JavaScript/1-Javascript/2-ES6/07-2-This.js
@@ -0,0 +1,213 @@
+///////////////////////////////////////////////////////
+//                     this Keyword
+///////////////////////////////////////////////////////
+
+
+//-----------------------
+// What is this?
+//-----------------------
+/*
+
+In JavaScript, the this keyword refers to an `object`
+Which object depends on how this is being invoked (used or called)
+
+
+The this keyword refers to different objects depending on how it is used:
+
+    If `this` is used in:                       `this` refers to the
+    -----------------------                     ------------------------------------------
+    object method                               object
+    Alone                                       global object
+    function (basic mode)                       global object
+    function (strict mode)                      this is undefined
+    event                                       element that received the event
+    Methods like (call(), apply(), bind() )     can refer to any object
+        
+        
+Note :: this is not a variable. It is a keyword. You cannot change the value of this.
+
+*/
+
+
+//---------------------
+// this in a Method
+//---------------------
+// When used in an object method, this refers to the object.
+
+
+// In this case, this refers to the person object.
+// Because the fullName method is a method of the person object.
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+};
+
+
+
+//---------------------
+// this Alone
+//---------------------
+// When used alone, `this` refers to the global object.
+
+// Because `this` is running in the global scope.
+// In a browser window the global object is [object Window]
+
+let x = this;   // x: [object Window]
+
+
+//  In strict mode
+// when used alone, this also refers to the global object
+"use strict";
+let y = this;   // y: [object Window]
+
+
+
+//---------------------------------
+// this in a Function (Default)
+//---------------------------------
+// In a function, the `global object` is the default binding for `this`
+// In a browser window the `global object` is [object Window]
+
+function myFunction() {
+    return this;    // [object Window]
+}
+
+
+
+//---------------------------------
+// this in a Function (Strict)
+//---------------------------------
+// JavaScript strict mode does not allow default binding.
+
+// So, when used in a function, in strict mode, `this` is `undefined`
+
+"use strict";
+function myFunction() {
+  return this;              // undefined
+}
+
+
+//---------------------------------
+// this in a Event Handlers
+//---------------------------------
+// In HTML event handlers, `this` refers to the `HTML element` that received the event:
+
+/*
+
+<button onclick="this.style.display='none'">
+  Click to Remove Me!
+</button>
+
+*/
+
+
+//---------------------------------
+// Object Method Binding
+//---------------------------------
+
+// In these examples, `this` is the person1 object:
+const person1 = {
+    firstName  : "John",
+    lastName   : "Doe",
+    id         : 5566,
+    myFunction : function() {
+      return this;
+    }
+};
+
+// this.firstName is the firstName property of this (the person2 object)
+const person2 = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+
+
+
+//---------------------------------
+// Explicit Function Binding
+//---------------------------------
+
+// The call() and apply() methods are predefined JavaScript methods.
+// They can both be used to call an object method with another object as argument.
+
+
+// The example below calls person3.fullName with person4 as an argument, 
+// `this` refers to person4, even if fullName is a method of person3:
+
+const person3 = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  const person4 = {
+    firstName:"John",
+    lastName: "Doe",
+  }
+  
+  // Return "John Doe":
+  person3.fullName.call(person4);
+
+
+
+//---------------------------------
+// Function Borrowing
+//---------------------------------
+// With the bind() method, an object can borrow a method from another object.
+
+
+// This example creates 2 objects (person and member).
+// The member object borrows the fullname method from the object personObj:
+
+const personObj = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+}
+  
+const member = {
+    firstName:"Hege",
+    lastName: "Nilsen",
+}
+  
+let fullName = personObj.fullName.bind(member);
+
+
+
+
+//---------------------------------
+// This Precedence
+//---------------------------------
+/*
+which object this refers to?
+
+use the following precedence to determine which object `this` refers to
+
+Precedence	    Object
+----------      --------------------
+1	            bind()
+2	            apply() , call()
+3	            Object method
+4	            Global scope
+
+
+1- Check to if `this` is in a function being called using bind() ?
+
+2- Check to if `this` is in a function being called using apply() ?
+
+3- Check to if `this` is in a function being called using call() ?
+
+4- Check to if `this` is in an object function (method) ?
+
+5- Check to if `this` is in a function in the global scope ?
