a6d425a94dffc753990fc3d0d6d885739a0c060f
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-03-05 15:24:48 +0330

    +

--- 2-DApp/1-JavaScript/1-Javascript/2-ES6/02-var-let-const.js
+++ 2-DApp/1-JavaScript/1-Javascript/2-ES6/02-var-let-const.js
@@ -2,14 +2,25 @@
 //                      Scope
 ///////////////////////////////////////////////////////
 
+// In ES6 we have three ways of declaring variables to store data values:
+
+var a = 'hello';
+const b = 'hello';
+let c = 'hello';
+
+// The type of declaration used depends on the necessary scope. 
+
+
+
 /*
 ------------------
 What is Scope
 ------------------
+Scope is the fundamental concept in all programming languages 
+
 Scope determines the accessibility (visibility) of variables from different parts of the code.
 
-// In JavaScript, objects and functions are also variables.
-
+NOTE :: In JavaScript, objects and functions are also variables.
 
 JavaScript has 3 types of scope :
 
@@ -24,12 +35,12 @@ JavaScript has 3 types of scope :
 ------------------
 Block Scope
 ------------------
-Before ES6 (2015), JavaScript had only 2 Scopes :
+Before ES6, JavaScript had only 2 Scopes :
 
     - Global 
     - Function
 
-ES6 introduced two important new JavaScript keywords :   
+ES6 (2015) introduced two important new JavaScript keywords :   
     - let   
     - const
 
@@ -38,7 +49,45 @@ These two keywords provide a new scope in JavaScript :
 */
 
 
+
+// difference between `var` , `let` , `const`
+
+
+// ---------- var ----------
+
+// Variables declared with the `var` can NOT have `block scope`
+// var defines a variable globally, or locally to an "entire function"
+// Variables declared inside a block, can be accessed from outside the block.
+
+{
+    var x = 2;
+}
+// x CAN be used here
+
+
+if (true) {
+    var x = 'Jack';
+}
+alert(x);    // OK
+
+
+
+// ---------- let ----------
+
+// allows you to declare variables that are limited in scope to the 
+//  block, statement, or expression in which they are used.
 // Variables declared inside a block cannot be accessed from outside the block:
+
+// x was declared as `var` and is accessible both inside and outside the if block    
+function varTest() {
+    var x = 1;
+    if (true) {
+        var x = 2;          // same variable
+        console.log(x);     // 2
+    }
+    console.log(x);         // 2
+}
+
 
 {
     let x = 2;
@@ -46,13 +95,102 @@ These two keywords provide a new scope in JavaScript :
 // x can NOT be used here
 
 
-// Variables declared with the `var` can NOT have `block scope`
-// Variables declared inside a block, can be accessed from outside the block.
-
-{
-    var x = 2;
+if (true) {
+    let x = 'Jack';
 }
-// x CAN be used here
+alert(x);    // x isn't reachable
+
+
+// x was declared as `let` and is accessible only in the scope of the if block    
+function letTest() {
+    let x = 1;
+    if (true) {
+        let x = 2;          // different variable
+        console.log(x);     // 2
+    }
+    console.log(x);         // 1
+}
+
+
+
+// Variables defined with let :
+
+//    - can be Reassigned new values.
+//    - but cannot be Redeclared (With var you can!)
+//    - must be Declared before use
+//    - have Block Scope
+
+
+let a = "Volvo";
+a = "Ford";
+let a; // ERROR
+
+
+
+// ---------- const ----------
+
+// `const` variables have the same scope as `let`
+
+/*
+The difference is that :
+
+    - const variables are immutable and are not allowed to be reassigned.
+        JavaScript const variables must be assigned a value when they are declared
+
+    - const variables cannot be Redeclared.
+
+    - const variables have Block Scope.
+*/
+
+const a = 'Hello';
+a = 'Bye';
+
+// ERROR:
+// TypeError: Assignment to constant variable.
+
+/*
+Use const when you declare:
+A new Array
+A new Object
+A new Function
+A new RegExp
+
+
+The keyword const is a little misleading. It does not define a constant value. 
+It defines a constant `reference` to a value.
+
+Because of this you can NOT:
+
+Reassign a constant value
+Reassign a constant array
+Reassign a constant object
+
+But you CAN:
+
+Change the elements of constant array
+Change the properties of constant object
+*/
+
+
+// The "const" keyword allows you to define a variable in a lexical scope
+// like with let
+// but you cannot reassign the value once one has been assigned.
+
+const pi = 3.14;
+
+pi = 4.13;          // ERROR :: you cannot reassign the value once one has been assigned.
+const pi = 4.13;    // ERROR :: You cannot re-declare a variable declared with const
+
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
+
+cars = ["Toyota", "Volvo", "Audi"];    // ERROR
 
 
 
@@ -108,6 +246,7 @@ function myFunction() {
 
 
 
+
 //---------------------
 //  Global Scope
 //---------------------
@@ -139,6 +278,11 @@ function myFunction() {
 var x = 2;       // Global scope
 let x = 2;       // Global scope
 const x = 2;     // Global scope
+// code here can use x
+
+function myFunction() {
+    // code here can also use x
+}
 
 
 // --- Global Variables in HTML ---
@@ -153,6 +297,7 @@ var carName = "Volvo";
 // Global variables defined with the let keyword do not belong to the window object:
 let carName = "Volvo";
 // code here can not use window.carName
+
 
 
 
@@ -179,186 +324,10 @@ function myFunction() {
 
 
 
+
 //------------------------
 // Variables Lifetime
 //------------------------
 // The lifetime of a JavaScript variable starts when it is declared.
 // Function (local) variables are deleted when the function is completed.
 // In a web browser, global variables are deleted when you close the browser window (or tab).
-
-
-
-
-
-/*
-    The let keyword was introduced in ES6 (2015).
-    In ES6 we have three ways of declaring variables to store data values:
-*/
-        var a = 10;
-        const b = 'hello';
-        let c = true;
-/*    
-    The type of declaration used depends on the necessary scope. 
-    Scope is the fundamental concept in all programming languages 
-    that defines the visibility of a variable.
-
-    Before ES6 (2015), JavaScript had only `Global Scope` and `Function Scope`
-    ES6 introduced two important new JavaScript keywords: let and const.
-    These two keywords provide `Block Scope` in JavaScript.
-    Variables declared inside a { } block cannot be accessed from outside the block!
-
-
-    var 
-        defines a variable globally, or locally to an "entire function"
-        regardless of block scope
-    
-    let 
-        allows you to declare variables that are limited in scope 
-        to the block, statement, or expression in which they are used.
-*/
-
-
-        // example:
-        if (true) {
-            let name = 'Jack';
-        }
-        // name isn't reachable
-        alert(name);
-
-
-/*
-    // Example :  difference in scope between ``var`` and ``let``
-
-    In this case, the `x` variable is accessible only in the scope of the if statement 
-       because it was declared as `let`
-
-*/
-
-        function varTest() {
-            var x = 1;
-            if (true) {
-                var x = 2;          // same variable
-                console.log(x);     // 2
-            }
-            console.log(x);         // 2
-        }
-
-        function letTest() {
-            let x = 1;
-            if (true) {
-                let x = 2;          // different variable
-                console.log(x);     // 2
-            }
-            console.log(x);         // 1
-        }
-
-        varTest();
-        letTest();
-
-
-
-/*
-        Variables defined with let :
-        
-            cannot be Redeclared  (With var you can!)
-            must be Declared before use
-            have Block Scope
-*/
-
-
-
-
-/*
-    -------------------
-    const
-    -------------------
-    The const keyword was introduced in ES6 (2015).
-
-    const variables have the same scope as variables declared using let.
-
-    The difference is that :
-        - const variables are immutable and are not allowed to be reassigned.
-            JavaScript const variables must be assigned a value when they are declared:
-        - const variables cannot be Redeclared.
-        - const variables have Block Scope.
-
-
-    For example, the following generates an exception:
-*/
-
-        const a = 'Hello';
-        a = 'Bye';
-
-        // ERROR:
-        // TypeError: Assignment to constant variable.
- 
-/*
-    const is not subject to Variable Hoisting too, 
-    which means that const declarations do not move to the top of the current execution context.
-
-    Also note that ES6 code will run only in browsers that support it. 
-    Older devices and browsers that do not support ES6 will return a syntax error.
-
-    Use const when you declare:
-        A new Array
-        A new Object
-        A new Function
-        A new RegExp
-
-
-    The keyword const is a little misleading.
-    It does not define a constant value. 
-        It defines a constant `reference` to a value.
-
-    Because of this you can NOT:
-
-        Reassign a constant value
-        Reassign a constant array
-        Reassign a constant object
-
-    But you CAN:
-
-        Change the elements of constant array
-        Change the properties of constant object
-*/
-
-
-
-    //-----------------------------
-    //  Examples
-    //-----------------------------
-    
-    // The "let" keyword allows you to define variables in a lexical scope, 
-    // as opposed to a function scope like the var keyword does.
-    let myVar = "Billy";
-
-    // Variables defined with let can be reassigned new values.
-    myVar = "William";
-
-    // The "const" keyword allows you to define a variable in a lexical scope
-    // like with let, but you cannot reassign the value once one has been assigned.
-
-    const pi = 3.14;
-
-    pi = 4.13; // You cannot do this.
-
-    // You can create a constant array:
-    const cars = ["Saab", "Volvo", "BMW"];
-
-    // You can change an element:
-    cars[0] = "Toyota";
-
-    // You can add an element:
-    cars.push("Audi");
-
-    const cars = ["Saab", "Volvo", "BMW"];
-
-    cars = ["Toyota", "Volvo", "Audi"];    // ERROR
-
-
-//You cannot re-declare a variable declared with let or const.
-
-    // This will not work:
-
-    let carName = "Volvo";
-    let carName;
