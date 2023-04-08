e03f1c839f55003dc76f3c1ac70d620f3b8d3b77
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-04-08 23:11:11 +0430

    +

--- BC10/Week5/02-MultiSigWallet/multiSigWallet.sol
+++ BC10/Week5/02-MultiSigWallet/multiSigWallet.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.22 <0.9.0;
 
+
 /**
     ------------------
     Multi-Sig Wallet
@@ -218,6 +219,7 @@ contract Target {
         return address(this).balance;
     }
 }
+
 
 /*
     1. Constructor()
