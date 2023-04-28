ae6a673067a2646a7d27ca6b57f68701ee5cf187
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-04-28 17:48:50 +0430

    +

---
+++ 1-Solidity/5-Frameworks-and-Libraries/01-Frameworks/01-Truffle/Sample-Projects/FirstTruffleProject/truffle-config.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const {INFURA_API_KEY, PRIVATE_KEY} = process.env;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    dashboard: {},
+    sepolia: {
+      provider: () => new HDWalletProvider(PRIVATE_KEY, INFURA_API_KEY),
+      network_id: '11155111',
+      gas: 4465030
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.13"
+    }
+  }
+};
