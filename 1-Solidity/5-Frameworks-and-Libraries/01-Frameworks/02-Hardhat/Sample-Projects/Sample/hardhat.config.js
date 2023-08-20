91f416cba5bbc7a3cf20f8ffb7747f8ddb3f19ea
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-08-20 12:39:27 +0330

    +

--- 1-Solidity/5-Frameworks-and-Libraries/01-Frameworks/02-Hardhat/Sample-Projects/Sample/hardhat.config.js
+++ 1-Solidity/5-Frameworks-and-Libraries/01-Frameworks/02-Hardhat/Sample-Projects/Sample/hardhat.config.js
@@ -2,6 +2,15 @@
  @type import('hardhat/config').HardhatUserConfig 
  */
 
+ require("@nomiclabs/hardhat-waffle");
+
+ task("accounts", "Print the list of accounts", async () => {
+  const accounts = await ElementInternals.getSigners();
+  for(const acc of accounts) {
+    console.log(acc,address);
+  }
+ });
+
 module.exports = {
   solidity: "0.8.19",
 };
