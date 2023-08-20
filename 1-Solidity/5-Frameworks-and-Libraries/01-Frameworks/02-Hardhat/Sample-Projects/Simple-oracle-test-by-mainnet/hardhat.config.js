8b1e154f1794f30ec5fb19666c718be2e4c9b591
Majid Shobeiri <shobeiri.majid@gmail.com>
2023-08-20 14:00:31 +0330

    +

--- 1-Solidity/5-Frameworks-and-Libraries/01-Frameworks/02-Hardhat/Sample-Projects/Simple-oracle-test-by-mainnet/hardhat.config.js
+++ 1-Solidity/5-Frameworks-and-Libraries/01-Frameworks/02-Hardhat/Sample-Projects/Simple-oracle-test-by-mainnet/hardhat.config.js
@@ -1,6 +1,7 @@
  require("@nomiclabs/hardhat-waffle");
  require('dotenv').config();
 
+ // This is a sample Hardhat task to get list of the hardhat node account.
  task("accounts", "Print the list of accounts", async () => {
   const accounts = await ethers.getSigners();
   for(const acc of accounts) {
@@ -8,6 +9,7 @@
   }
  });
 
+// You need to export an object to set up your config
 module.exports = {
   solidity: "0.8.19",
   networks: {
